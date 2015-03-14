abbb711b9a06c2ffbc672319e35a74d7fdb46790
mmalfertheiner <martin.malfertheiner@student.uibk.ac.at>
2015-03-14 12:15:19 +0100

    Initial netbeans setup.

---
+++ Compiler/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: martin
+ *
+ * Created on 14. März 2015, 12:10
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
