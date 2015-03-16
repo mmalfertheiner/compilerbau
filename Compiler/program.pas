157e033656e8af0942aa550ea3ee525797d696de
Valentin Huber <valentin.huber@gmail.com>
2015-03-16 22:40:38 +0100

    Added further 50 lines and 5 errors. Added <procedure> to the tokens

--- Compiler/program.pas
+++ Compiler/program.pas
@@ -6,9 +6,11 @@ program exercise8;
 
 	begin
 
+
 	{ Bubble sort initialization }
 	var numbers array [1..100] of real
-	var i, j, temp : integer;
+	var i, j, #temp : integer;
+
 
 	{ Bubble sort implementation }
 
@@ -16,7 +18,7 @@ program exercise8;
 		for j := 2 to i do
 			if (numbers[j-1] > numbers[j]) then
 		begin
-			temp := numbers[j-1];
+			?temp := numbers[j-1];
 			numbers[j-1] := numbers[j];
 			numbers[j] := temp;
 		end;
@@ -26,17 +28,75 @@ program exercise8;
     var numbers array [1..100] of real
 	var i, j, temp : integer;
 
+
 	{ Insertion sort implementation }
 	for i := 2 to size-1 do
 		begin
-			index := numbers[i];
+			§index := numbers[i];
 			j := i;
 			while ((j > 1) and (numbers[j-1] > index)) do
 			begin
-				numbers[j] := numbers[j-1];
+				önumbers[j] := numbers[j-1];
 				j := j - 1;
 			end;
 				numbers[j] := index;
 		end;
+
+
+	procedure quicksort (l, r : integer);
+	{ Quicksort initialization }
+	var i, j, v, t : integer;
+
+
+	{ Insertion sort implementation }
+	begin
+		if r > l then
+		begin
+			v := A[r];
+			i := l - 1;
+			j := r;
+			repeat
+				repeat 
+					i := i + 1
+				until A[i] >= v;
+				repeat
+					j := j - 1
+				until A[j] <= v;
+				t := A[i];
+				A[i] := A[r];
+				A[r] := t;
+			$quicksort (l , i - 1);
+			quicksort (i + 1, r)
+			end
+		end;
+
+
+	procedure shellsort;
+	{ Shellsort initialization }
+	var i, j, h, value : %integer;
+
+	{ Shellsort implementation }
+	begin
+	h := 1;
+	repeat
+		h := (3 * h) + 1 {This can be changed, but this is a good formula.}
+	until h > N;
+	repeat
+		h := h div 3;
+		for i := h + 1 to N do
+		begin
+			value := A[i];
+			j := i;
+			while A[j - h] > value do
+				begin
+					A[j] := A[j - h];
+					j := j - h;
+					if j <= h then
+					goto 0
+				end;
+			A[j] := vale
+		end
+	until h = 1
+	end;
 
 	end.
